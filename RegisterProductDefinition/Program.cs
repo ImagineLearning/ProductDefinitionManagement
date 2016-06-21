cc285023e369e893cf96442584073a6356338b3d
Matt Lund <matt.lund@imaginelearning.com>
2016-06-21 13:07:30 -0600

    Finished up utility to upload a helix exported product definition

--- RegisterProductDefinition/Program.cs
+++ RegisterProductDefinition/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -20,36 +22,79 @@ namespace RegisterProductDefinition
 
         static void Main(string[] args)
         {
-            // Configure log4net based on the contents of the App.config
-            XmlConfigurator.Configure();
-            
-	        Parameters parameters = new Parameters();
-	        bool success = CommandLine.Parser.Default.ParseArguments(args, parameters);
+            const int failureExistCode = -1;
 
-	        if (!success)
-	            Environment.Exit(-1);
-
-            string blobName = parameters.Product.ToLowerInvariant() + "-" + parameters.Revision.ToLowerInvariant() + ".zip";
-
-            string blobContainerConnectionString = ConfigurationManager.AppSettings["BlobConnectionString"];
-            CloudBlobContainer cloudBlobContainer = new CloudBlobContainer(new Uri(blobContainerConnectionString));
-
-            if (cloudBlobContainer.GetBlobReference(blobName).Exists())
+            try
             {
-                Log.Info(blobName+" already exists in the blob container");
-                Environment.Exit(-1);
-            }
-            else
-            {
-                CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference(blobName);
-                Log.Info("Attempting to upload " + blobName);
+                // Configure log4net based on the contents of the App.config
+                XmlConfigurator.Configure();
 
-                using (var fileStream = System.IO.File.OpenRead(parameters.FullPath))
+                Parameters parameters = new Parameters();
+                bool success = CommandLine.Parser.Default.ParseArguments(args, parameters);
+
+                if (!success)
+                    Environment.Exit(failureExistCode);
+
+                if (parameters.Product.Length < 1)
                 {
-                    blockBlob.UploadFromStream(fileStream, AccessCondition.GenerateIfNotExistsCondition());
+                    Console.WriteLine("Product identifier's length must be >= 1 ");
+                    Environment.Exit(failureExistCode);
                 }
 
-                Log.Info(blobName + " uploaded.");
+                if (parameters.Revision.Length < 1)
+                {
+                    Console.WriteLine("Revision's length must be >= 1");
+                    Environment.Exit(failureExistCode);
+                }
+
+                string blobName = parameters.Product.ToLowerInvariant() + "-" + parameters.Revision.ToLowerInvariant() +
+                                  ".zip";
+
+                string blobContainerConnectionString = ConfigurationManager.AppSettings["BlobConnectionString"];
+                CloudBlobContainer cloudBlobContainer = new CloudBlobContainer(new Uri(blobContainerConnectionString));
+
+                if (cloudBlobContainer.GetBlobReference(blobName).Exists())
+                {
+                    Console.WriteLine(blobName + " already exists in the blob container");
+                    Environment.Exit(failureExistCode);
+                }
+                else
+                {
+                    CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference(blobName);
+                    Log.Info("Attempting to upload " + blobName);
+
+                    if (!File.Exists(parameters.FullPath))
+                    {
+                        Console.WriteLine("File not found [" + parameters.FullPath + "]");
+                        Environment.Exit(failureExistCode);
+                    }
+
+                    if (!parameters.FullPath.ToLowerInvariant().EndsWith(".zip"))
+                    {
+                        Console.WriteLine("Product definition must be a .zip file");
+                        Environment.Exit(failureExistCode);
+                    }
+
+                    using (var fileStream = System.IO.File.OpenRead(parameters.FullPath))
+                    {
+                        blockBlob.UploadFromStream(fileStream, AccessCondition.GenerateIfNotExistsCondition());
+                    }
+
+                    Log.Info(blobName + " uploaded.");
+                }
+            }
+            catch (StorageException exception)
+            {
+                if (exception.Message.Contains("403"))
+                {
+                    Console.WriteLine("FAILURE: The authentication key that was used to upload the zip file is invalid (rejected).  Please talk to Grady/Matt/Devin about resolving this issue");
+                    Environment.Exit(failureExistCode);
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.Fatal(exception.Message, exception);
+                Environment.Exit(failureExistCode);
             }
         }
     }
@@ -62,7 +107,7 @@ namespace RegisterProductDefinition
 		[Option("product", Required = true, HelpText = "Product identifier such as ILE")]
 		public string Product { get; set; }
 
-		[Option("revision", Required = true, HelpText = "Revision, note this is a string so enclose numbers in double quotes such as \"123\"")]
+		[Option("revision", Required = true, HelpText = "Revision.  Must be unique within the space of revisions for this particular product identifier")]
 		public string Revision { get; set; }
 
 		[HelpOption]
